d6b20776ea78300bd7839f8b6c2fbd3968ef65d3
jose barrios <thetreeman2003@gmail.com>
2021-03-17 09:25:26 -0700

    Create minPriorityQueue.py

---
+++ minPriorityQueue.py
@@ -0,0 +1,48 @@
+"""
+File: minPriorityQueue.py
+Author: Jose Barrios
+
+Resources:
+
+    - https://www.w3schools.com/python/python_reference.asp
+
+"""
+
+import math
+import sys
+
+""" - - - - - - - - - - - - Exercise 2 - - - - - - - - - - - - """
+
+class MinPriorityQueue:
+	
+	def __init__(self):
+		
+		self.arr = []
+		self.heapSize = 0
+		
+	def size(self):
+		
+		return self.heapSize
+		
+	def getMin(self):
+	
+		pass
+		
+	def extractMin(self):
+	
+		pass
+		
+	def insert(self, x):
+	
+		self.heapSize += 1
+		self.arr[self.heapSize] = int(sys.maxsize)
+		i = self.heapSize
+		if x > A[i]:
+		
+			print("Given key is larger than current key, try again.")
+			
+		A[i] = key
+		while i > 1 and A[math.floor(i/2)] > A[i]:
+		
+			A[i], A[math.floor(i/2)] = A[math.floor(i/2)], A[i]
+			i = math.floor(i/2)
