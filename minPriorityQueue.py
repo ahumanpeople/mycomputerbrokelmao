8a4e66326d8e1acdaf8afa74f2c4f71fea61d24a
jose barrios <thetreeman2003@gmail.com>
2021-03-17 13:37:01 -0700

    Update minPriorityQueue.py

--- minPriorityQueue.py
+++ minPriorityQueue.py
@@ -1,11 +1,8 @@
 """
 File: minPriorityQueue.py
 Author: Jose Barrios
-
 Resources:
-
     - https://www.w3schools.com/python/python_reference.asp
-
 """
 
 import math
@@ -16,63 +13,107 @@ import sys
 class MinPriorityQueue:
 	
 	def __init__(self):
-		
-		self.arr = []
+		# initializes the list that will contain the heap as well as the heapSize variable
+		self.arr = [None]
 		self.heapSize = 0
 		
 	def size(self):
-		
+		# returns heapSize
 		return self.heapSize
 		
 	def getMin(self):
-	
-		return self.arr[1]
+		# returns the minimum value, or the root of the heap
+		return self.arr[0]
 	
 	def minHeapify(self, i):
-		
+		# set l as i's left child, r as i's right child, and smallest as i
 		l, r, smallest = 2*i, (2*i)+1, i
-		
-		if l <= self.heapSize and A[l] < A[i]: smallest = l
-		elif r <= self.heapSize and A[r] < A[i]: smallest = r
-			
+		# if l is in the heap and its value is smaller than i's, then l is the smallest
+		if l <= self.heapSize and self.arr[l] < self.arr[i]: smallest = l
+		# if r is in the heap and its value is smaller than i's, then r is the smallest
+		elif r <= self.heapSize and self.arr[r] < self.arr[i]: smallest = r
+		# if the smallest isn't i, but rather one of its children
 		if smallest != i:
-			
-			A[i], A[smallest] = A[smallest], A[i]
+			# swap the smallest child with i and then continue to min heapify the smallest's subtree
+			self.arr[i], self.arr[smallest] = self.arr[smallest], self.arr[i]
 			self.minHeapify(smallest)
 		
 	def extractMin(self):
-	
+		# if there is a heap
 		if self.heapSize >= 1:
-			
-			minimum = self.arr[1]
-			self.arr[1] = self.arr[self.heapSize]
+			# set the minimum to the minimum variable
+			minimum = self.arr[0]
+			# set the minimum position equal to the largest value in the heap
+			self.arr[0] = self.arr[self.heapSize - 1]
+			# shrink the heap by one
 			self.heapSize -= 1
-			self.minHeapify(1)
+			# and then heapify the entire heap, and return the extracted minimum value
+			self.minHeapify(0)
 			return minimum
-		
+		# if there isn't a heap
 		else:
-			
+			# notify the user and return None
 			print("Queue is empty.")
 			return None
 		
 	def insert(self, x):
-	
-		self.heapSize += 1
-		i, self.arr[self.heapSize] = self.heapSize, x
-		while i > 1 and self.arr[math.floor(i/2)] > self.arr[i]:
-		
-			self.arr[i], self.arr[math.floor(i/2)] = self.arr[math.floor(i/2)], self.arr[i]
-			i = math.floor(i/2)
+	    # set i equal to the heapSize, and then increment the heap
+	    i = self.heapSize
+	    self.heapSize += 1
+	    # adds the new key to the heap, removing the None placeholder or elements not in the heap but in the list if needed
+	    if self.arr[0] == None: self.arr[0] = x
+		elif len(self.arr) >= i: self.arr[i - 1] = x
+	    else: self.arr.append(x)
+	    # while i is bigger (or equal to) 1 and i's parent is bigger than i
+	    while i >= 1 and self.arr[int(math.floor(i/2))] > self.arr[i]:
+	        # swap i and its parent's positions, and then make i equal its parent
+	        self.arr[i], self.arr[int(math.floor(i/2))] = self.arr[int(math.floor(i/2))], self.arr[i]
+	        i = int(math.floor(i/2))
 			
 	def __str__(self):
-		
-		return self.arr
+		# makes separate list, since all of the list might not be all of the heap
+		printList = []
+		for i in range(self.heapSize):
+		    # adds elements to the new list while they're still part of the heap
+		    printList.append(self.arr[i])
+		# then return the separate list
+		return str(printList)
 			
 testQueue = MinPriorityQueue()
 testQueue.insert(6)
 testQueue.insert(7)
+testQueue.insert(15)
 testQueue.insert(12)
 testQueue.insert(10)
 testQueue.insert(17)
-testQueue.insert(15)
 print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()) + "\n")
+testQueue.extractMin()
+print(testQueue)
+print("Heap Size: " + str(testQueue.size()))
+print("Min Value: " + str(testQueue.getMin()))
